680757c9071f9a44363eb777d153711f506624a5
Fajtai Andris <andris.fajkusz@gmail.com>
2022-02-08 20:18:41 +0100

    Interval defs

--- theories/Interval.v
+++ theories/Interval.v
@@ -11,14 +11,33 @@ Inductive intervalQuality : Type :=
   | iqual : intervalCategory -> Z -> intervalQuality.
 
 Definition P     := iqual perfect     0.
-(*TODO constantok helyett notation?*)
-Notation "'Perfect'" := (iqual perfect 0) (at level 80, right associativity).
 Definition Aug   := iqual perfect     1.
 Definition Dim   := iqual perfect  (- 1).
 Definition major := iqual majorminor    0.
 Definition minor := iqual majorminor (- 1).
 Definition aug   := iqual majorminor    1.
 Definition dim   := iqual majorminor (- 2).
+(*TODO constantok helyett notation?*)
+Notation "'Perfect'"    := (iqual perfect 0) (at level 80, right associativity).
+Notation "'Augmented'"  := (iqual perfect 1) (at level 80, right associativity).
+Notation "'Diminished'" := (iqual perfect (- 1)) (at level 80, right associativity).
+Notation "'major'"      := (iqual majorminor 0) (at level 80, right associativity).
+Notation "'minor'"      := (iqual majorminor (- 1)) (at level 80, right associativity).
+Notation "'augmented'"  := (iqual majorminor 1) (at level 80, right associativity).
+Notation "'diminished'" := (iqual majorminor (- 2)) (at level 80, right associativity).
+
+Fixpoint category_from_number (n : nat) :=
+  match n with
+  | 0 => perfect
+  | 1 => perfect
+  | 2 => majorminor
+  | 3 => majorminor
+  | 4 => perfect
+  | 5 => perfect
+  | 6 => majorminor
+  | 7 => majorminor
+  | S(S(S(S(S(S(S n)))))) => category_from_number n
+  end.
 
 Definition modifier (q : intervalQuality) : Z :=
   match q with 
@@ -29,8 +48,6 @@ Inductive intervalName : Type :=
   | iname : intervalQuality -> nat -> intervalName.
 
 (*TODO what about iname P 2 ? Also P 0*)
-(*TODO ez kell?*)
-Notation "! Q N" := (iname Q N) (at level 80, right associativity).
 
 Definition size (i : intervalName) : Z :=
   match i with
@@ -40,13 +57,56 @@ Definition size (i : intervalName) : Z :=
     + 12 * (Z.div (Z.of_nat n) 8)
   end.
 
-Definition between_pitchClasses (x y : pitchClass) : intervalName :=
+Definition between_pitch_classes (x y : pitchClass) : intervalName :=
   match x, y with
   | l1 # m1 , l2 # m2 =>
-    iname (iqual perfect 0) (Letter.upward_distance l1 l2)
+    iname
+    (iqual
+    (*categoey*)
+    (category_from_number (1 + next_count l1 l2))
+    (*modifier*) 
+    ((Z.of_nat(Letter.upward_distance l1 l2))
+    -(Z.of_nat(Letter.upward_distance C (nextN C (next_count l1 l2))))
+    + m2 - m1))
+    (*number*)
+    (1 + next_count l1 l2)
   end.
 
-Eval compute in between_pitchClasses (C # 0) (G # 0).
+Inductive directionalIntervalName : Type :=
+  | upward : intervalName -> directionalIntervalName
+  | downward : intervalName -> directionalIntervalName.
+
+Definition between_pitches (x y : pitch) : directionalIntervalName :=
+  if Z.leb (distance_C0 x) (distance_C0 y)
+  then match x, y with
+    | l1 # m1 ' o1, l2 # m2 ' o2 => 
+      match (between_pitch_classes (l1 # m1) (l2 # m2)) with
+      | iname q n => 
+        if (*We leaped over a C letter*)
+        andb (Nat.ltb (Letter.upward_distance l1 C) (Letter.upward_distance l2 C))
+        (negb (eqb l1 C))
+        then upward (iname q (n + (o2 - o1 - 1) * 7))
+        else if andb (eqb l2 C) (negb (eqb l1 C)) (*This could be simplified logically*)
+          then upward (iname q (n + (o2 - o1 - 1) * 7))
+          else upward (iname q (n + (o2 - o1) * 7))
+      end
+    end(*CODE BELOW IS COPIED FROM ABOVE*)
+  else match x, y with
+    | l2 # m2 ' o2, l1 # m1 ' o1 => 
+      match (between_pitch_classes (l1 # m1) (l2 # m2)) with
+      | iname q n => 
+        if
+        andb (Nat.ltb (Letter.upward_distance l1 C) (Letter.upward_distance l2 C))
+        (negb (eqb l1 C))
+        then downward (iname q (n + (o2 - o1 - 1) * 7))
+        else if andb (eqb l2 C) (negb (eqb l1 C))
+          then downward (iname q (n + (o2 - o1 - 1) * 7))
+          else downward (iname q (n + (o2 - o1) * 7))
+      end
+    end
+.
+
+Eval compute in between_pitches (D # 0 ' 4) (B # 0 ' 3). 
 
 (*
 Definition apply_to_pitchClass (p : pitchClass) (i : interval_name) : pitchClass := 
@@ -56,6 +116,7 @@ Definition apply_to_pitchClass (p : pitchClass) (i : interval_name) : pitchClass
   (*Modifier*) # m + (size i - size (iname
             (*   - Z.of_nat (PitchClass.upward_distance (l # 0) (nextN l (n - 1) # 0))*)
   end.
+
 Eval compute in apply_to_pitchClass (A # 0) (iname major 18).
 
 Definition apply_to_pitch (p : pitch) (i : interval_name) : pitch := 
--- theories/Letter.v
+++ theories/Letter.v
@@ -13,11 +13,38 @@ Definition next (x : letter) : letter :=
   | G => A
   end.
 
+Definition eqb (x y : letter) : bool :=
+  match x, y with
+  | A, A => true
+  | B, B => true
+  | C, C => true
+  | D, D => true
+  | E, E => true
+  | F, F => true
+  | G, G => true
+  | _, _ => false
+  end.
+
 Fixpoint nextN (x : letter) (n : nat) : letter :=
   match n with
   | 0 => x
   | S n => nextN (next x) n
   end.
+
+(*not very sophisticated*)
+(*n is expected to be 6 to work properly.*)
+Fixpoint next_count_rec (x y : letter) (n : nat) : nat :=
+  match n with
+  | 0 => 0
+  | S n => if eqb (nextN x (S n)) (y)
+           then (S n)
+           else next_count_rec x y n
+  end.
+
+Definition next_count (x y : letter) : nat :=
+  if eqb x y
+  then 0
+  else next_count_rec x y 6.
 
 Definition upward_distance_from_A (x : letter) : nat :=
   match x with
