2c29f0d4b949c112284eadebeba05d229ea0e9ac
Fajtai Andris <andris.fajkusz@gmail.com>
2022-03-09 16:38:34 +0100

    midi parsing

---
+++ midi_parser/README.md
@@ -0,0 +1,19 @@
+# MIDI parser
+
+This python script is meant for easily converting MIDI files to Bremen Coq source code.  
+
+## Függőségek
+ - Mido 1.2.10  
+ - Music21
+
+## Usage
+
+Printing Coq source code:  
+```shell
+$ python parser.py example.mid
+```
+
+Saving Coq source code:  
+```shell
+$ python parser.py example.mid example.v
+```
--- midi_parser/parser.py
+++ midi_parser/parser.py
@@ -1,21 +1,78 @@
 #!/usr/bin/python
 
 import sys
-from mido import MidiFile
+import mido
+from music21 import pitch
 
 def parse (midifile) :
-    mid = MidiFile(midifile)
-    output = ""
+  mid = mido.MidiFile(midifile)
+  preparsed_midi = []
+  if len(mid.tracks) != 1:
+    raise Exception("Multitrack input is not supported!")
+  else:
     for i, track in enumerate(mid.tracks):
-        output += ('Track {}: {}'.format(i, track.name)) + "\n"
-        for msg in track:
-            output += str(msg) + "\n"
-    return output
+      for msg in track:
+        msgdict = msg.dict()
+        if msgdict.get('type') == 'note_on':
+          note = str(pitch.Pitch(msgdict.get('note')))
+          event = 'start' if msgdict.get('velocity') != 0 else 'stop'
+          deltasec = msgdict.get('time')
+          preparsed_midi.append([event, note, deltasec])
+
+    parsed_midi = []
+    next_is_start = True
+    legato_ratio = 0.1
+    
+    for i in range(len(preparsed_midi)):
+      current = preparsed_midi[i]
+      if next_is_start and current[0] == 'stop':
+        raise Exception("Polyphonic melody is not supported!")
+      else:
+        next_is_start = not next_is_start
+        if i != 0:
+          previous = preparsed_midi[i-1]
+          #hangok közötti rövid szünetek összeolvasztása a korábbi hanggal
+          if current[0] == 'start' and current[2] != 0:
+            if current[2] < previous[2] * legato_ratio:
+              previous[2] += current[2]
+              current[2] = 0
+            else:
+            #szünetek megállapítása
+              current[1] = 'rest'
+              parsed_midi.append(current)
+          else: 
+            parsed_midi.append(current)
+
+    #event címkék törlése
+    notes = []
+    for i in parsed_midi:
+      notes.append([i[1], i[2]])
+    
+    #legkissebb egység
+    min_delta = 999999
+    for i in notes:
+      if i[1] != 0 and i[1] < min_delta: min_delta = i[1]
+  
+    # INNENTŐL FELTESZEM, HOGY A min_delta 2-ES LEOSZTÁSÚ HANGOT TALÁLT
+    #hanghosszúság arányok számítása
+        
+    for i in notes:
+      i[1] = round((i[1] / min_delta)*2)
+    
+    
+    output = ""
+    for i in notes:
+      output += i[0] + ' ' + str(i[1]) + '\n'
+  return output
 
 
 if len(sys.argv) == 1:
-    print('Missing argument!')
+  print('Missing argument!')
 elif len(sys.argv) == 2:
-    print(parse(sys.argv[1]))
+  print(parse(sys.argv[1]))
+elif len(sys.argv) == 3:
+  f = open(sys.argv[2], "w")
+  f.write(parse(sys.argv[1]))
+  f.close()
 else:
-    print('Too many arguments!')
+  print('Too many arguments!')
